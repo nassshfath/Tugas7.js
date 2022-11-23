cfc835c057a61fa8b78526d520e3ac6eb683c1c8
Gendrenk <nasrullohfath@gmail.com>
2022-11-23 10:14:38 +0700

    Tugas 7 JS

---
+++ Tugas7.js
@@ -0,0 +1,10 @@
+var tinggiBadan = [161,162,163,164,165,166,167,168,169,170];
+    for(var i=0; i<tinggiBadan.length; i++){
+        console.log(i)
+    }
+
+    console.log()
+
+    for(let x of tinggiBadan){
+        console.log(x);
+    }
